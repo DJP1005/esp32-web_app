a9595e866b646f6b8eaf1e2111280936595cb04d
DJP1005 <154476853+DJP1005@users.noreply.github.com>
2024-12-16 13:21:06 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,6 @@ self.addEventListener('notificationclick', function(event) {
     console.log('Powiadomienie kliknięte:', event.notification.tag);
     event.notification.close();
     event.waitUntil(
-        clients.openWindow('https://twoja-strona.pl') // Link do otwarcia po kliknięciu
+        clients.openWindow('https://djp1005.github.io/esp32-web_app/') // Link do otwarcia po kliknięciu
     );
 });
